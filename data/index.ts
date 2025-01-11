27d497dde6aebbf2b5fa3657590de0b4abd722f8
xavier bahegne <xavierbahegne@gmail.com>
2025-01-11 18:18:15 +0100

    add the project

---
+++ data/index.ts
@@ -0,0 +1,223 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    // {
+    //   id: 6,
+    //   title: "I prioritize client collaboration, fostering open communication ",
+    //   description: "",
+    //   className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    //   imgClassName: "w-full h-full",
+    //   titleClassName: "justify-end",
+    //   img: "/b1.svg",
+    //   spareImg: "",
+    // },
+    {
+      id: 1,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Disponible partout dans le monde",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Mon tech stack",
+      description: "je me forme aux nouvelles technologies au quotidien",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    // {
+    //   id: 4,
+    //   title: "Tech enthusiast with a passion for development.",
+    //   description: "",
+    //   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    //   imgClassName: "",
+    //   titleClassName: "justify-start",
+    //   img: "/grid.svg",
+    //   spareImg: "/b4.svg",
+    // },
+  
+    // {
+    //   id: 5,
+    //   title: "Currently building a JS Animation library",
+    //   description: "The Inside Scoop",
+    //   className: "md:col-span-3 md:row-span-2",
+    //   imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    //   titleClassName: "justify-center md:justify-start lg:justify-center",
+    //   img: "/b5.svg",
+    //   spareImg: "/grid.svg",
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/adrianhajdin?tab=repositories",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/zoom-clone",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
+  
+  export const testimonials = [
+{
+  quote:
+    "Collaborer avec Xavier a été une expérience exceptionnelle. Son professionnalisme, sa rapidité et son souci du détail ont permis de mener notre projet à bien. Grâce à son expertise en développement, notre site web a atteint un tout autre niveau. Je le recommande vivement !",
+  name: "Marie Dubois",
+  title: "Responsable Marketing chez NovaCorp",
+  img: "/profile1.svg",
+},
+{
+  quote:
+    "Xavier est un développeur passionné et extrêmement compétent. Son approche structurée et créative a transformé notre vision en un site web performant et esthétique. Travailler avec lui a été un véritable plaisir.",
+  name: "Jean-Claude Martin",
+  title: "CEO de BrightTech",
+  img: "/profile2.svg",
+},
+{
+  quote:
+    "Xavier a surpassé toutes nos attentes. Sa capacité à comprendre nos besoins, à proposer des solutions innovantes et à respecter les délais a fait toute la différence. Si vous recherchez un développeur talentueux, ne cherchez pas plus loin !",
+  name: "Claire Simon",
+  title: "Directrice de Projet chez WebSolutions",
+  img: "/profile3.svg",
+},
+{
+  quote:
+    "Avec Xavier, nous avons eu un partenaire dévoué et passionné par son métier. Sa maîtrise des technologies de développement et son sens de la collaboration nous ont permis de mener notre projet au succès.",
+  name: "Alexandre Lefèvre",
+  title: "CTO de GreenFuture",
+  img: "/profile4.svg",
+},
+{
+  quote:
+    "Xavier est bien plus qu’un simple développeur : c’est un véritable architecte de solutions numériques. Grâce à son expertise, notre entreprise a pu se démarquer avec un site web moderne et performant. Je le recommande sans hésiter.",
+  name: "Sophie Bernard",
+  title: "Fondatrice de StartUp Innovate",
+  img: "/profile5.svg",
+},
+
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
